6f16ae29259b6eca0123cea08e2ea53da196a504
tsuraj <stsurajdeep490@gmail.com>
2025-09-07 12:28:02 +0530

    Implement: support custom delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,8 +3,18 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ""
   
-    #split on comma or newline
-    numbers.split(/,|\n/).map { |n| n.to_i }.reduce(0, :+)
+    delimiter_regex = /,|\n/
+    nums = numbers
+  
+    if nums.start_with?("//")
+      header, rest = nums.split("\n", 2)
+      custom = header[2..-1] # after //
+      
+      delimiter_regex = Regexp.new(Regexp.escape(custom))
+      nums = rest || ""
+    end
+  
+    nums.split(delimiter_regex).map { |n| n.to_i }.reduce(0, :+)
   end
 end
   
