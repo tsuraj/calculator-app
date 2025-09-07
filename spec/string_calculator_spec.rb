844100c651592daafb9a91c35d39bff875e3f724
tsuraj <stsurajdeep490@gmail.com>
2025-09-07 12:04:59 +0530

    Add test: custom  delimiter test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ RSpec.describe StringCalculator do
     it "handles newlines between numbers" do
       expect(calc.add("1\n2,3")).to eq(6)
     end
+    
+    it "supports custom single-character delimiter with header" do
+      expect(calc.add("//;\n1;2")).to eq(3)
+    end
   end
 end
