a55a3ceaac1bb41e8562448bce21097e6c7e3d93
tsuraj <stsurajdeep490@gmail.com>
2025-09-07 10:52:00 +0530

    Implement: support newline as delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,10 @@
+# lib/string_calculator.rb
 class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ""
-    numbers.split(",").map { |n| n.to_i }.reduce(0, :+)
-   end
+  
+    #split on comma or newline
+    numbers.split(/,|\n/).map { |n| n.to_i }.reduce(0, :+)
+  end
 end
   
